16ce6ac0deb6e1129621101f3b353005eb984e37
Alexander Neubeck <aneubeck@github.com>
2024-10-30 12:50:33 +0000

    Implement a first 4-ary trie to represent posting lists. This is our base-line implementation.

---
+++ crates/quaternary_trie/src/lib.rs
@@ -0,0 +1,509 @@
+use virtual_bitrank::VirtualBitRank;
+
+mod virtual_bitrank;
+
+const MAX_LEVEL: usize = 14;
+
+pub struct QuarternaryTrie {
+    data: VirtualBitRank,
+    /// Total number of nibbles on each level.
+    level_idx: [usize; MAX_LEVEL],
+}
+
+/// Level: 0 ==> ........xx
+/// Level: 1 ==> ......xx..
+/// Level: 2 ==> ....xx....
+///              ^^^^
+///             prefix
+///                  ^^
+///              nibble bit
+/// Van Emde Boas layout/traversal
+///           1
+///         /    \
+///       2        3
+///     /  \      /  \
+///    4    7    a    d
+///   / \  / \  / \  / \
+///   5 6  8 9  b c  e f
+///
+/// Process: 123, 489, 5ab, 6cd, 7ef
+/// 0xxx 00xx 01xx | STOP & Recurse
+/// 000x 0000 0001
+/// 001x 0010 0011
+/// 010x 0100 0101
+/// 011x 0110 0111
+///
+/// now with two bit levels
+///
+/// 00xxxxxx 01xxxxxx 10xxxxxx 11xxxxxx
+/// 0000xxxx 0001xxxx 0010xxxx 0011xxxx
+/// 0100xxxx 0101xxxx 0110xxxx 0111xxxx
+/// 1000xxxx 1001xxxx 1010xxxx 1011xxxx
+/// 1100xxxx 1101xxxx 1110xxxx 1111xxxx
+///
+/// Stop and recurse
+///
+/// 000000xx 000001xx 000010xx 000011xx
+/// 00000000 00000001 00000010 00000011
+/// 00000100 00000101 00000110 00000111
+/// 00001000 00001001 00001010 00001011
+/// 00001100 00001101 00001110 00001111
+///
+/// 000100xx 000101xx 000110xx 000111xx
+/// 00010000 00010001 00010010 00010011
+/// 00010100 00010101 00010110 00010111
+/// 00011000 00011001 00011010 00011011
+/// 00011100 00011101 00011110 00011111
+///
+/// 001000xx 001001xx 001010xx 001011xx
+/// 00100000 00100001 00100010 00100011
+/// 00100100 00100101 00100110 00100111
+/// 00101000 00101001 00101010 00101011
+/// 00101100 00101101 00101110 00101111
+///
+/// 001100xx 001101xx 001110xx 001111xx
+/// 00110000 00110001 00110010 00110011
+/// 00110100 00110101 00110110 00110111
+/// 00111000 00111001 00111010 00111011
+/// 00111100 00111101 00111110 00111111
+///
+/// 010000xx 010001xx 010010xx 010011xx
+/// ...
+///
+/// Process first half:
+/// xxxxxxxx--------
+/// Reset and process second half
+/// 00000000xxxxxxxx
+
+fn get_prefix(value: u32, level: usize) -> u32 {
+    if level == 16 {
+        0
+    } else {
+        value >> (level * 2)
+    }
+}
+
+fn gallopping_search<F: Fn(u32) -> bool>(values: &mut &[u32], f: F) {
+    let mut step = 1;
+    while step < values.len() {
+        if f(values[step]) {
+            break;
+        }
+        *values = &mut &values[step..];
+        step *= 2;
+    }
+    step /= 2;
+    while step > 0 {
+        if step < values.len() && !f(values[step]) {
+            *values = &mut &values[step..];
+        }
+        step /= 2;
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Layout {
+    Linear,
+    VanEmdeBoas,
+    DepthFirst,
+}
+
+impl QuarternaryTrie {
+    fn count_levels(&mut self, values: &mut &[u32], level: usize) -> bool {
+        let prefix = values[0] >> (level * 2 + 2);
+        let mut nibble = 0;
+        let mut all_set = true;
+        while !values.is_empty() && values[0] >> (level * 2 + 2) == prefix {
+            nibble |= 1 << ((values[0] >> level * 2) & 3);
+            if level > 0 {
+                all_set &= self.count_levels(values, level - 1);
+            } else {
+                *values = &values[1..];
+            }
+        }
+        all_set &= nibble == 15;
+        self.level_idx[level] += 1;
+        all_set
+    }
+
+    fn van_emde_boas(&mut self, values: &mut &[u32], level: usize, res: usize) {
+        let prefix = get_prefix(values[0], level);
+        if res == 0 {
+            let mut nibble = 0;
+            while !values.is_empty() && get_prefix(values[0], level) == prefix {
+                let v = values[0] >> (level * 2 - 2);
+                nibble |= 1 << (v & 3);
+                *values = &mut &values[1..];
+                gallopping_search(values, |x| x >> (level * 2 - 2) > v);
+            }
+            if level <= MAX_LEVEL {
+                self.data.set_nibble(self.level_idx[level - 1], nibble);
+                self.level_idx[level - 1] += 1;
+            }
+            return;
+        }
+        // process level .. level - res
+        // This level has to be processed at half resolution
+        let mut copy = &values[..];
+        self.van_emde_boas(&mut copy, level, res / 2);
+        // Then process level - res..level - 2 *res
+        // Process all the children within this subtree.
+        while !values.is_empty() && get_prefix(values[0], level) == prefix {
+            self.van_emde_boas(values, level - res, res / 2);
+        }
+    }
+
+    fn fill_bit_rank(&mut self, values: &mut &[u32], level: usize) -> bool {
+        let prefix = values[0] >> (level * 2 + 2);
+        let mut nibble = 0;
+        let mut all_set = true;
+        while !values.is_empty() && values[0] >> (level * 2 + 2) == prefix {
+            nibble |= 1 << ((values[0] >> level * 2) & 3);
+            if level > 0 {
+                all_set &= self.fill_bit_rank(values, level - 1);
+            } else {
+                *values = &values[1..];
+            }
+        }
+        all_set &= nibble == 15;
+        self.data.set_nibble(self.level_idx[level], nibble);
+        self.level_idx[level] += 1;
+        all_set
+    }
+
+    pub fn new(values: &[u32], layout: Layout) -> Self {
+        let mut s = Self {
+            data: VirtualBitRank::new(),
+            level_idx: [0; MAX_LEVEL],
+        };
+        let mut consumed = values;
+        s.count_levels(&mut consumed, MAX_LEVEL - 1);
+        if matches!(layout, Layout::Linear) {
+            s.data.reserve(s.level_idx.iter().sum::<usize>() * 4);
+        }
+        s.level_idx
+            .iter_mut()
+            .rev()
+            .scan(0, |acc, x| {
+                let old = *acc;
+                *acc = *acc + *x;
+                *x = old;
+                Some(old)
+            })
+            .skip(usize::MAX)
+            .next();
+        let mut consumed = values;
+        if matches!(layout, Layout::VanEmdeBoas | Layout::Linear) {
+            s.van_emde_boas(&mut consumed, 16, 8);
+        } else {
+            s.fill_bit_rank(&mut consumed, MAX_LEVEL - 1);
+        }
+        s.data.build();
+        s.reset_stats();
+        println!(
+            "encoded size: {}",
+            4.0 * s.level_idx[0] as f32 / values.len() as f32
+        );
+        s
+    }
+
+    fn reset_stats(&mut self) {
+        self.data.reset_stats();
+    }
+
+    fn page_count(&self) -> (usize, usize) {
+        self.data.page_count()
+    }
+
+    fn recurse(&self, node: usize, level: usize, value: u32, results: &mut Vec<u32>) {
+        if level == 1 {
+            self.recurse2(node, value, results);
+        } else {
+            let n = self.data.get_nibble(node);
+            let value = value * 4;
+            if n == 0 {
+                results.extend((0..4 << (level * 2)).map(|i| (value << (level * 2)) + i));
+                return;
+            }
+            let mut r = self.data.rank(node * 4) as usize;
+            if n & 1 != 0 {
+                r += 1;
+                self.recurse(r, level - 1, value, results);
+            }
+            if n & 2 != 0 {
+                r += 1;
+                self.recurse(r, level - 1, value + 1, results);
+            }
+            if n & 4 != 0 {
+                r += 1;
+                self.recurse(r, level - 1, value + 2, results);
+            }
+            if n & 8 != 0 {
+                r += 1;
+                self.recurse(r, level - 1, value + 3, results);
+            }
+        }
+    }
+
+    #[inline(always)]
+    fn recurse2(&self, node: usize, value: u32, results: &mut Vec<u32>) {
+        let n = self.data.get_nibble(node);
+        let value = value * 4;
+        if n == 0 {
+            results.extend((0..16).map(|i| (value << 2) + i));
+            return;
+        }
+        let mut r = self.data.rank(node * 4) as usize;
+        if n & 1 != 0 {
+            r += 1;
+            self.recurse0(r, value, results);
+        }
+        if n & 2 != 0 {
+            r += 1;
+            self.recurse0(r, value + 1, results);
+        }
+        if n & 4 != 0 {
+            r += 1;
+            self.recurse0(r, value + 2, results);
+        }
+        if n & 8 != 0 {
+            r += 1;
+            self.recurse0(r, value + 3, results);
+        }
+    }
+
+    #[inline(always)]
+    fn recurse0(&self, node: usize, value: u32, results: &mut Vec<u32>) {
+        let n = self.data.get_nibble(node);
+        let value = value * 4;
+        if n == 0 {
+            results.extend((0..4).map(|i| value + i));
+            return;
+        }
+        if n & 1 != 0 {
+            results.push(value);
+        }
+        if n & 2 != 0 {
+            results.push(value + 1);
+        }
+        if n & 4 != 0 {
+            results.push(value + 2);
+        }
+        if n & 8 != 0 {
+            results.push(value + 3);
+        }
+    }
+
+    pub fn collect(&self) -> Vec<u32> {
+        let mut results = Vec::with_capacity(self.level_idx[0]);
+        self.recurse(0, MAX_LEVEL - 1, 0, &mut results);
+        results
+    }
+}
+
+pub trait TrieIteratorTrait {
+    fn get(&self, level: usize) -> u32;
+    fn down(&mut self, level: usize, child: u32);
+}
+
+pub struct TrieTraversal<'a> {
+    trie: &'a QuarternaryTrie,
+    pos: [u32; MAX_LEVEL],
+}
+
+impl<'a> TrieTraversal<'a> {
+    pub fn new(bpt: &'a QuarternaryTrie) -> Self {
+        Self {
+            trie: bpt,
+            pos: [0; MAX_LEVEL],
+        }
+    }
+}
+
+impl TrieIteratorTrait for TrieTraversal<'_> {
+    fn get(&self, level: usize) -> u32 {
+        self.trie.data.get_nibble(self.pos[level] as usize)
+    }
+
+    fn down(&mut self, level: usize, child: u32) {
+        let index = self.pos[level] * 4 + child;
+        let new_index = self.trie.data.rank(index as usize + 1);
+        self.pos[level - 1] = new_index as u32;
+    }
+}
+
+pub struct TrieIterator<T> {
+    trie: T,
+    level: usize,
+    item: u32,
+    nibbles: [u32; MAX_LEVEL],
+}
+
+impl<T: TrieIteratorTrait> TrieIterator<T> {
+    pub fn new(trie: T) -> Self {
+        let mut result = Self {
+            trie,
+            level: MAX_LEVEL - 1,
+            item: 0,
+            nibbles: [0; MAX_LEVEL],
+        };
+        result.nibbles[result.level] = result.trie.get(result.level);
+        result
+    }
+}
+
+impl<'a, T: TrieIteratorTrait> Iterator for TrieIterator<T> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        while self.level < MAX_LEVEL {
+            let child = (self.item >> (2 * self.level)) & 3;
+            let nibble = self.nibbles[self.level] >> child;
+            if nibble != 0 {
+                let delta = nibble.trailing_zeros();
+                if self.level == 0 {
+                    let res = self.item + delta;
+                    self.item = res + 1;
+                    self.level = (self.item.trailing_zeros() / 2) as usize;
+                    return Some(res);
+                }
+                self.item += delta << (2 * self.level);
+                self.trie.down(self.level, child + delta);
+                self.level -= 1;
+                self.nibbles[self.level] = self.trie.get(self.level);
+            } else {
+                self.item |= 3 << (self.level * 2);
+                self.item += 1 << (self.level * 2);
+                self.level = (self.item.trailing_zeros() / 2) as usize;
+            }
+        }
+        None
+    }
+}
+
+pub struct Intersection<T> {
+    left: T,
+    right: T,
+}
+
+impl<T: TrieIteratorTrait> Intersection<T> {
+    pub fn new(left: T, right: T) -> Self {
+        Self { left, right }
+    }
+}
+
+impl<T: TrieIteratorTrait> TrieIteratorTrait for Intersection<T> {
+    fn get(&self, level: usize) -> u32 {
+        self.left.get(level) & self.right.get(level)
+    }
+
+    fn down(&mut self, level: usize, child: u32) {
+        self.left.down(level, child);
+        self.right.down(level, child);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Instant;
+
+    use itertools::{kmerge, Itertools};
+    use rand::{thread_rng, Rng};
+
+    use crate::{Intersection, Layout, QuarternaryTrie, TrieIterator, TrieTraversal};
+
+    #[test]
+    fn test_bpt() {
+        let values = vec![3, 6, 7, 10];
+        let trie = QuarternaryTrie::new(&values, Layout::VanEmdeBoas);
+        assert_eq!(trie.collect(), values);
+
+        let values: Vec<_> = (1..63).collect();
+        let trie = QuarternaryTrie::new(&values, Layout::VanEmdeBoas);
+        assert_eq!(trie.collect(), values);
+    }
+
+    #[test]
+    fn test_large() {
+        let mut values: Vec<_> = (0..10000000)
+            .map(|_| thread_rng().gen_range(0..100000000))
+            .collect();
+        values.sort();
+        values.dedup();
+
+        let start = Instant::now();
+        let trie = QuarternaryTrie::new(&values, Layout::VanEmdeBoas);
+        println!("construction {:?}", start.elapsed() / values.len() as u32);
+
+        let start = Instant::now();
+        let result = trie.collect();
+        println!("reconstruction {:?}", start.elapsed() / values.len() as u32);
+        assert_eq!(result, values);
+
+        let iter = TrieIterator::new(TrieTraversal::new(&trie));
+        let start = Instant::now();
+        let result: Vec<_> = iter.collect();
+        println!("iteration {:?}", start.elapsed() / values.len() as u32);
+        assert_eq!(result, values);
+    }
+
+    #[test]
+    fn test_van_emde_boas_layout() {
+        let values: Vec<_> = (0..64).collect();
+        let trie = QuarternaryTrie::new(&values, Layout::VanEmdeBoas);
+        assert_eq!(trie.collect(), values);
+    }
+
+    #[test]
+    fn test_intersection() {
+        let mut page_counts = [0, 0, 0];
+        for _ in 0..3 {
+            let mut values: Vec<_> = (0..100000)
+                .map(|_| thread_rng().gen_range(0..100000000))
+                .collect();
+            values.sort();
+            values.dedup();
+
+            let mut values2: Vec<_> = (0..100000000)
+                .map(|_| thread_rng().gen_range(0..100000000))
+                .collect();
+            values2.sort();
+            values2.dedup();
+
+            let start = Instant::now();
+            let intersection: Vec<_> = kmerge([values.iter(), values2.iter()])
+                .tuple_windows()
+                .filter_map(|(a, b)| if *a == *b { Some(*a) } else { None })
+                .collect();
+            println!(
+                "kmerge intersection {:?}",
+                start.elapsed() / values.len() as u32
+            );
+            println!("Intersection size: {}", intersection.len());
+
+            for (i, layout) in [Layout::VanEmdeBoas, Layout::DepthFirst, Layout::Linear]
+                .into_iter()
+                .enumerate()
+            {
+                let trie = QuarternaryTrie::new(&values, layout);
+                let trie2 = QuarternaryTrie::new(&values2, layout);
+                let iter = TrieIterator::new(Intersection::new(
+                    TrieTraversal::new(&trie),
+                    TrieTraversal::new(&trie2),
+                ));
+                let start = Instant::now();
+                let result: Vec<_> = iter.collect();
+                let count = trie.page_count();
+                let count2 = trie2.page_count();
+                page_counts[i] += count.0 + count.1;
+                println!(
+                    "trie intersection {:?} {}",
+                    start.elapsed() / values.len() as u32,
+                    (count.0 + count2.0) as f32 / (count.1 + count2.1) as f32
+                );
+                assert_eq!(result, intersection);
+            }
+            println!("{page_counts:?}");
+        }
+    }
+}
---
+++ crates/quaternary_trie/src/virtual_bitrank.rs
@@ -0,0 +1,301 @@
+/*
+   Layout:
+       Want <5% permutation table!
+       Max posting lists with 2^28 documents = ~90MB
+       NVMe page size is 4096 bytes
+       4 bytes for block offset ==> 4 billion blocks
+       counts
+           4 bytes ==> leads to overflow, but NOT within one posting list!
+           3 bytes only works for <16 million documents :(
+           Store count into middle of block
+       64 block size
+           index size: 64 * 4 billion ==> 256 GB
+           lookup overhead: 6.25%
+           blocks per page: 64 ==> can store about 4 layers within one page! ==> "worst case" 4 pages
+           count: 32 byte (AVX-2, 256-bits)
+               overhead of count: 6.25%
+               total overhead: 12.5% ==> not good to store within lookup table!
+       128 block size
+           index size: 128 * 4 billion ==> 512 GB
+           lookup overhead: 3.125%
+           blocks per page: 32 ==> can store at least 3 layers (of at most 14) within one page! ==> "worst case" 5 pages
+           count: 64 bytes (AVX-512, 512-bits)
+               overhead of count: 3.125%
+               total overhead: 6.25%
+               1 block = 4 * 32 counts ==> store counts inside of page?
+           sub-counts: 32-bytes
+               one-byte for each sub-count
+               two splits: --- 1 byte --- 4 bytes --- 1 byte ---
+               overhead of sub-counts: 1.5625%
+
+   Iteration:
+       basic: decode next 4 values on level L
+       decode ahead: decode next 16? values down to level L
+           decode on each previous level ~16 values ahead
+           not quite clear how to arrange the data best
+           16 values fit into one avx2 register
+*/
+
+use std::cell::RefCell;
+
+const BLOCK_BYTES: usize = 128;
+const BLOCK_BITS: usize = BLOCK_BYTES * 8;
+const PAGE_BYTES: usize = 4096;
+const PAGE_BITS: usize = PAGE_BYTES * 8;
+const BLOCKS_PER_PAGE: usize = PAGE_BYTES / BLOCK_BYTES;
+const WORD_BITS: usize = 64;
+const WORD_BYTES: usize = WORD_BITS / 8;
+const WORDS_PER_BLOCK: usize = BLOCK_BYTES / WORD_BYTES;
+
+#[repr(C, align(128))]
+#[derive(Default, Clone)]
+struct Block {
+    words: [u64; WORDS_PER_BLOCK],
+}
+
+#[derive(Default)]
+pub(crate) struct VirtualBitRank {
+    // In order to look up bit i, use block_mapping[i / BLOCK_BITS]
+    block_mapping: Vec<u32>,
+    blocks: Vec<Block>,
+    // Remember which pages have been accessed.
+    stats: Vec<RefCell<u64>>,
+}
+
+impl VirtualBitRank {
+    pub(crate) fn new() -> Self {
+        Self::with_capacity(0)
+    }
+
+    pub(crate) fn with_capacity(bits: usize) -> Self {
+        let bits = (bits + BLOCK_BITS - 1) & !(BLOCK_BITS - 1);
+        Self {
+            block_mapping: vec![0; bits / BLOCK_BITS], // 0 means unused block!!
+            blocks: Vec::with_capacity(bits / BLOCK_BITS),
+            stats: Vec::new(),
+        }
+    }
+
+    pub(crate) fn reset_stats(&mut self) {
+        self.stats = vec![
+            RefCell::new(0);
+            ((self.blocks.len() + BLOCKS_PER_PAGE - 1) / BLOCKS_PER_PAGE + 63) / 64
+        ];
+    }
+
+    pub(crate) fn page_count(&self) -> (usize, usize) {
+        (
+            self.stats
+                .iter()
+                .map(|v| v.borrow().count_ones() as usize)
+                .sum(),
+            (self.blocks.len() + BLOCKS_PER_PAGE - 1) / BLOCKS_PER_PAGE,
+        )
+    }
+
+    fn bit_to_block(&self, bit: usize) -> usize {
+        let result = self.block_mapping[bit / BLOCK_BITS] as usize;
+        /*if let Some(v) = self.stats.get(result / PAGE_BITS / 64) {
+            *v.borrow_mut() += 1 << (result % 64);
+        }*/
+        result
+    }
+
+    fn mid_rank(&self, block: usize) -> u32 {
+        let first_block = block & !(BLOCKS_PER_PAGE - 1);
+        let array = self.blocks[first_block].words.as_ptr() as *const u32;
+        unsafe { array.add(block & (BLOCKS_PER_PAGE - 1)).read() }
+    }
+
+    pub(crate) fn rank(&self, bit: usize) -> u32 {
+        let block = self.bit_to_block(bit);
+        let mut rank = self.mid_rank(block);
+        let word = (bit / WORD_BITS) & (WORDS_PER_BLOCK - 1);
+        let bit_in_word = bit & (WORD_BITS - 1);
+        if word >= WORDS_PER_BLOCK / 2 {
+            for i in WORDS_PER_BLOCK / 2..word {
+                rank += self.blocks[block].words[i].count_ones();
+            }
+            if bit_in_word != 0 {
+                rank + (self.blocks[block].words[word] << (WORD_BITS - bit_in_word)).count_ones()
+            } else {
+                rank
+            }
+        } else {
+            for i in word + 1..WORDS_PER_BLOCK / 2 {
+                rank -= self.blocks[block].words[i].count_ones();
+            }
+            rank - (self.blocks[block].words[word] >> bit_in_word).count_ones()
+        }
+    }
+
+    pub(crate) fn reserve(&mut self, bits: usize) {
+        assert!(self.block_mapping.is_empty());
+        assert!(self.blocks.is_empty());
+        // let pages = (bits + PAGE_BITS - 1) / PAGE_BITS;
+        let blocks = (bits + BLOCK_BITS - 1) / BLOCK_BITS;
+        for _ in 0..blocks {
+            if self.blocks.len() % BLOCKS_PER_PAGE == 0 {
+                self.blocks.push(Block::default());
+            }
+            self.block_mapping.push(self.blocks.len() as u32);
+            self.blocks.push(Block::default());
+        }
+    }
+
+    fn get_word_mut(&mut self, bit: usize) -> &mut u64 {
+        let block = bit / BLOCK_BITS;
+        if block >= self.block_mapping.len() {
+            self.block_mapping.resize(block + 1, 0);
+        }
+        if self.block_mapping[block] == 0 {
+            if self.blocks.len() % BLOCKS_PER_PAGE == 0 {
+                self.blocks.push(Block::default()); // Block with rank information
+            }
+            self.block_mapping[block] = self.blocks.len() as u32;
+            self.blocks.push(Block::default());
+        }
+        let block = self.bit_to_block(bit);
+        let word = (bit / WORD_BITS) & (WORDS_PER_BLOCK - 1);
+        &mut self.blocks[block].words[word]
+    }
+
+    pub(crate) fn set(&mut self, bit: usize) {
+        *self.get_word_mut(bit) |= 1 << (bit & (WORD_BITS - 1));
+    }
+
+    pub(crate) fn set_nibble(&mut self, nibble_idx: usize, nibble_value: u32) {
+        let bit_idx = nibble_idx * 4;
+        // clear all bits...
+        // *self.get_word(bit_idx) &= !(15 << (bit_idx & (WORD_BITS - 1)));
+        *self.get_word_mut(bit_idx) |= (nibble_value as u64) << (bit_idx & (WORD_BITS - 1));
+    }
+
+    pub(crate) fn build(&mut self) {
+        for block in &mut self.block_mapping {
+            if *block == 0 {
+                if self.blocks.len() % BLOCKS_PER_PAGE == 0 {
+                    self.blocks.push(Block::default()); // Block with rank information
+                }
+                *block = self.blocks.len() as u32;
+                self.blocks.push(Block::default());
+            }
+        }
+        let mut ones = 0;
+        for block in 0..self.block_mapping.len() {
+            let block = self.block_mapping[block] as usize;
+            for i in 0..WORDS_PER_BLOCK / 2 {
+                ones += self.blocks[block].words[i].count_ones();
+            }
+            unsafe {
+                let first_block = block & !(BLOCKS_PER_PAGE - 1);
+                let array = self.blocks[first_block].words.as_mut_ptr() as *mut u32;
+                let rank = array.add(block & (BLOCKS_PER_PAGE - 1));
+                rank.write(ones);
+            }
+            for i in WORDS_PER_BLOCK / 2..WORDS_PER_BLOCK {
+                ones += self.blocks[block].words[i].count_ones();
+            }
+        }
+    }
+
+    pub(crate) fn get(&self, bit: usize) -> bool {
+        let block = self.bit_to_block(bit);
+        let word = (bit / WORD_BITS) & (WORDS_PER_BLOCK - 1);
+        let bit_in_word = bit & (WORD_BITS - 1);
+        self.blocks[block].words[word] & (1 << bit_in_word) != 0
+    }
+
+    pub(crate) fn get_nibble(&self, nibble_idx: usize) -> u32 {
+        let bit_idx = nibble_idx * 4;
+        let block = self.bit_to_block(bit_idx);
+        let word = (bit_idx / WORD_BITS) & (WORDS_PER_BLOCK - 1);
+        let bit_in_word = bit_idx & (WORD_BITS - 1);
+        ((self.blocks[block].words[word] >> bit_in_word) & 15) as u32
+    }
+
+    fn len(&self) -> usize {
+        self.block_mapping.len() * BLOCK_BITS
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Instant;
+
+    use rand::{seq::SliceRandom, thread_rng, RngCore};
+
+    use super::{VirtualBitRank, BLOCK_BITS, WORD_BITS};
+
+    #[test]
+    fn test_bitrank() {
+        let mut bitrank = VirtualBitRank::with_capacity(1 << 20);
+        let mut rank = vec![];
+        let mut bits = vec![];
+        let mut ones = 0;
+        for i in 0..bitrank.len() {
+            let bit = thread_rng().next_u32() % 2 == 1;
+            rank.push(ones);
+            bits.push(bit);
+            if bit {
+                bitrank.set(i);
+                ones += 1;
+            }
+        }
+        bitrank.build();
+        for (i, bit) in bits.iter().enumerate() {
+            assert_eq!(bitrank.get(i), *bit, "at position {i}");
+        }
+        for (i, r) in rank.iter().enumerate() {
+            assert_eq!(bitrank.rank(i), *r, "at position {i}");
+        }
+    }
+
+    /// This test emulates the reordering of blocks on disk.
+    /// With a real NVMe, the performance difference should be much larger.
+    /// But even this basic test shows that the access pattern matters.
+    /// I.e. throughput is  15% higher when access order is non-random.
+    #[test]
+    fn test_random_order() {
+        let mut bitrank = VirtualBitRank::with_capacity(1 << 20);
+        let random_bits: Vec<_> = (0..bitrank.len() / WORD_BITS)
+            .map(|_| thread_rng().next_u32())
+            .flat_map(|i| {
+                [i, i + 1].into_iter().map(|i| {
+                    (i & !(BLOCK_BITS - 1) as u32) % bitrank.len() as u32
+                    //(i & !(BLOCK_BITS - 1) as u32 + BLOCK_BITS as u32 / 2) % bitrank.len() as u32
+                    // i % bitrank.len() as u32
+                })
+            })
+            .collect();
+        for i in &random_bits {
+            bitrank.set(*i as usize);
+        }
+        bitrank.build();
+
+        let mut sorted_bits = random_bits.clone();
+        sorted_bits.shuffle(&mut thread_rng());
+
+        for _ in 0..4 {
+            let time = Instant::now();
+            for i in &random_bits {
+                assert!(bitrank.get(*i as usize), "at position {i}");
+            }
+            println!(
+                "time to check random bits: {:?} {:?}",
+                time.elapsed(),
+                time.elapsed() * 100 / random_bits.len() as u32
+            );
+
+            let time = Instant::now();
+            for i in &sorted_bits {
+                assert!(bitrank.get(*i as usize), "at position {i}");
+            }
+            println!(
+                "time to check sorted bits: {:?} {:?}",
+                time.elapsed(),
+                time.elapsed() * 100 / random_bits.len() as u32
+            );
+        }
+    }
+}
