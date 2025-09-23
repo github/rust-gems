8e6f9b0c41c2f1c29d542fcf6d67d8531cf98daf
Alexander Neubeck <aneubeck@github.com>
2025-09-23 09:54:40 +0200

    baseline

---
+++ crates/posting-lists/src/lib.rs
@@ -0,0 +1,162 @@
+pub struct TebIterator<'a> {
+    data: &'a [u64],
+    index: u32,
+    bit_pos: u32,
+
+    ranges: [u32; 8],
+    range_idx: u32,
+}
+
+impl<'a> TebIterator<'a> {
+    pub fn new(data: &'a [u64]) -> Self {
+        Self {
+            data,
+            index: 0,
+            bit_pos: 0,
+            ranges: Default::default(),
+            range_idx: 0,
+        }
+    }
+
+    pub fn decode_batch(&mut self) {
+        let mut data = self.data[(self.bit_pos / 64) as usize];
+        //while data != 0 {
+        for _ in 0..13 {
+            let level = self.index.trailing_zeros();
+            let a = data ^ data.saturating_sub(1);
+            let down = a.count_ones();
+            let end = self.index + (1 << (level + 1 - down));
+            let b = (data >> down) as u32 & 1;
+            self.ranges[self.range_idx as usize] = self.index;
+            self.range_idx += (self.range_idx & 1) ^ b;
+            self.bit_pos += down + 1;
+            self.index = end;
+            data >>= down + 1;
+        }
+    }
+}
+
+struct BitStream {
+    data: Vec<u32>,
+    bit_pos: u32,
+    curr: u64,
+}
+
+impl BitStream {
+    fn new() -> Self {
+        Self {
+            data: vec![],
+            bit_pos: 0,
+            curr: 0,
+        }
+    }
+
+    fn push(&mut self, bits: u32, num_bits: u32) {
+        self.curr |= (bits as u64) << (self.bit_pos % 32);
+        self.bit_pos += num_bits;
+        if self.bit_pos % 64 >= 32 {
+            self.data.push(self.curr as u32);
+            self.curr >>= 32;
+            self.bit_pos -= 32;
+        }
+    }
+
+    fn finish(mut self) -> (Vec<u64>, u32) {
+        if self.bit_pos % 32 != 0 {
+            self.data.push(self.curr as u32);
+        }
+        if self.data.len() % 2 != 0 {
+            self.data.push(0);
+        }
+        let (head, _) = self.data.as_slice().as_chunks::<2>();
+        let data = head
+            .iter()
+            .map(|[a, b]| (*b as u64) << 32 | *a as u64)
+            .collect::<Vec<_>>();
+        (data, self.bit_pos)
+    }
+}
+
+struct Encoder {
+    index: u32,
+    out: BitStream,
+}
+
+impl Encoder {
+    fn new() -> Self {
+        Encoder {
+            index: 0,
+            out: BitStream::new(),
+        }
+    }
+
+    fn encode_till(&mut self, end: u32, flag: bool) {
+        let mask = ((flag as u32) << 1) | 1;
+        while self.index < end {
+            let level = self.index.trailing_zeros();
+            let rest = self.index ^ end;
+            if level < 32 && rest >= (1 << level) {
+                self.out.push(mask, 2);
+                self.index += 1 << level;
+            } else {
+                let down = 31 - rest.leading_zeros();
+                self.index += 1 << down;
+                self.out.push(mask << (level - down), level - down + 2);
+            }
+        }
+    }
+
+    fn encode(mut self, ranges: &[u32]) -> (Vec<u64>, u32) {
+        for [start, end] in ranges.as_chunks().0 {
+            self.encode_till(*start, false);
+            self.encode_till(*end, true);
+        }
+        self.encode_till(1 << 31, false);
+        self.out.finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encoding() {
+        let encoder = Encoder::new();
+        let seq = [2, 3, 5, 7, 10, 15];
+        let postings = seq.as_chunks().0.iter().map(|[a, b]| *b - *a).sum::<u32>();
+        let (data, _) = encoder.encode(&seq);
+        println!("{:064b} {:064b}", data[1], data[0]);
+
+        let mut iter = TebIterator::new(&data);
+        iter.decode_batch();
+        assert_eq!(&iter.ranges[..iter.range_idx as usize], &seq);
+
+        let start = std::time::Instant::now();
+        let iterations = 1000;
+        for _ in 0..iterations {
+            std::hint::black_box({
+                let mut iter = TebIterator::new(&data);
+                iter.decode_batch();
+            });
+        }
+        let duration = start.elapsed();
+        println!(
+            "Decoding took: {:?} {:?} {:?}",
+            iterations as f32 / duration.as_secs_f32(),
+            postings as f32 * iterations as f32 / duration.as_secs_f32(),
+            duration / 13 / iterations
+        );
+    }
+
+    #[test]
+    fn test_decoding() {
+        let data = [
+            0b11111110100000010101110011000000,
+            0b00110011001100110011001100110011,
+        ];
+        let mut iter = TebIterator::new(&data);
+        iter.decode_batch();
+        assert_eq!(iter.ranges[..iter.range_idx as usize], [0, 4, 8, 12]);
+    }
+}
